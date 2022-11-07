b7e5371b0c989ee991952d41aab43c64cd2d4f90
eric911115 <aeric911115@gmail.com>
2022-11-07 20:59:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,47 +1,34 @@
-from flask import Flask, request, abort
+from importlib.resources import contents
+import requests 
+from bs4 import BeautifulSoup
+import urllib.request as req
+import time
+import threading
 
-from linebot import (
-    LineBotApi, WebhookHandler
-)
-from linebot.exceptions import (
-    InvalidSignatureError
-)
-from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,
-)
+def set_interval(func, sec):
+    def func_wrapper():
+        set_interval(func, sec)
+        func()
+    t = threading.Timer(sec, func_wrapper)
+    t.start()
+    return t
 
-app = Flask(__name__)
+    
 
-line_bot_api = LineBotApi('zegN/VBfBTlX9qAQoGJWWMazAUWyEN/DI+kPR+lqIaoWTamaIZq59REhdduCL1n+tQsjHPrWXpMckk+gOGON/coXA9Bx47cVJoDxBbeA3fLlxA3TwdYW/u9yPM3IWm0NoMoE1I4hfXDC+OR0viORlwdB04t89/1O/w1cDnyilFU=')
-handler = WebhookHandler('f7daa972ca14bb6c060aec3245beb7e9')
-
-@app.route("/callback", methods=['POST'])
-def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-
-    # get request body as text
-    body = request.get_data(as_text=True)
-    print("Request body: " + body, "Signature: " + signature)
-
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-
-    return 'OK'
-
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    #print("Handle: reply_token: " + event.reply_token + ", message: " + event.message.text)
-    content = "{}: {}".format(event.source.user_id, event.message.text)
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=content))
-
-import os
-if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 80))
-    app.run(host='0.0.0.0', port=port)
+url="https://www.fight30.com/products/mango-jump-towel"
+    
+with req.urlopen(url)as response:
+    data=response.read().decode("utf-8")
+ 
+root=BeautifulSoup(data,"html.parser")
+titles=root.find("div",class_="out-of-stock txt-sold-out")
+if titles.text == "售完":
+    headers = {
+        "Authorization": "Bearer " + "NpxGoV7UPJ0GT0jRWChIvVuqg6hKJU7QTms3vCW6i64",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+    params = {"message":"芒果醬毛巾有貨了 速速購買\nhttps://www.fight30.com/products/mango-jump-towel"}
+    r = requests.post("https://notify-api.line.me/api/notify",
+                      headers=headers, params=params)
+    print(r.status_code)  #200
+  
